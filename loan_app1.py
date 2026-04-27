f701d44eb4e408424dc3f28ed13f7079dafead15
Elias Hilliard <eliashilliard1@gmail.com>
2026-04-27 10:57:25 -0400

    Refactor input encoding by removing model column checks
    Removed unused model column handling for input encoding.

--- loan_app1.py
+++ loan_app1.py
@@ -47,12 +47,6 @@ input_data = pd.DataFrame([{
 input_encoded = pd.get_dummies(input_data)
 
 
-model_cols = model.feature_names_in_
-
-for col in model_cols:
-    if col not in input_encoded.columns:
-        input_encoded[col] = 0
-
 input_encoded = input_encoded[model_cols]
 
 #Prediction
